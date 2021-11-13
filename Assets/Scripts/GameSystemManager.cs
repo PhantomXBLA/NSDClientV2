6e2646aabe0b637f6f118d0f0d136457c3b48c37
Tyler <phantomxbla@gmail.com>
2021-11-13 17:42:14 -0500

    imported main menu label, got references to all login elements in GameSystemManager, made toggle boxes toggle the other one when turned off

--- Assets/Scripts/GameSystemManager.cs
+++ Assets/Scripts/GameSystemManager.cs
@@ -1,23 +1,75 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameSystemManager : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    GameObject submitButton;
-    GameObject usernameInput;
-    GameObject passwordInput;
+    GameObject SubmitButton;
+    GameObject UsernameInput;
+    GameObject PasswordInput;
+
+    GameObject LoginToggle;
+    GameObject CreateAccountToggle;
 
     void Start()
     {
         GameObject[] allObjects = UnityEngine.Object.FindObjectsOfType<GameObject>();
+
+        foreach(GameObject go in allObjects)
+        {
+            if (go.name == "UsernameInput")
+            {
+                UsernameInput = go;
+            }
+
+            else if (go.name == "PasswordInput")
+            {
+                PasswordInput = go;
+            }
+
+            else if (go.name == "SubmitButton")
+            {
+                SubmitButton = go;
+            }
+
+            else if (go.name == "LoginToggle")
+            {
+                LoginToggle = go;
+            }
+
+            else if (go.name == "CreateAccountToggle")
+            {
+                CreateAccountToggle = go;
+            }
+        }
+
+        SubmitButton.GetComponent<Button>().onClick.AddListener(OnSubmitButtonPressed);
+        LoginToggle.GetComponent<Toggle>().onValueChanged.AddListener(LoginToggleChanged);
+        CreateAccountToggle.GetComponent<Toggle>().onValueChanged.AddListener(AccountToggleChanged);
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void OnSubmitButtonPressed()
+    {
+        Debug.Log("button pressed");
+    }
+
+    public void LoginToggleChanged(bool changedValue)
+    {
+        CreateAccountToggle.GetComponent<Toggle>().SetIsOnWithoutNotify(!changedValue);
+    }
+
+    public void AccountToggleChanged(bool changedValue)
+    {
+        LoginToggle.GetComponent<Toggle>().SetIsOnWithoutNotify(!changedValue);
     }
 }
